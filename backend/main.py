f19605acf7f0423b148613953829a9ddbdb2ac88
rushdhamv <rushdha52@gmail.com>
2023-11-09 23:31:10 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -11,13 +11,30 @@ app.secret_key="rushdha"
 # to config particular data
 #app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://username:password@localhost/databasename'
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:@localhost/slot'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/slot'
 db=SQLAlchemy(app)
 
+class Test(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50))
+    
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# testing whether db is connected or not
+
+@app.route("/test")
+def test():
+    try:
+        a=Test.query.all()
+        print(a)
+        return 'mydatabase is connected'
+    except Exception as e:
+        print(e)
+        return f'mydatabase is not connected {e}'
+
 
 
 app.run(debug=True)
