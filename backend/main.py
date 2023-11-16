6d97c99cb63504b58836d12a42fd36913ae2f7ea
rushdhamv <rushdha52@gmail.com>
2023-11-16 21:11:56 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from flask import Flask,redirect,render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from flask_login import login_required,logout_user,login_user,login_manager,LoginManager,current_user 
+from flask_login import login_required,logout_user,login_user,login_manager,LoginManager,current_user
+from sqlalchemy import text
+
 from werkzeug.security import generate_password_hash,check_password_hash
+
 
 # mydatabase connection
 #helloo
@@ -11,25 +14,22 @@ app=Flask(__name__)
 app.secret_key="rushdha"
 
 
-# this is for getting the unique user access
-login_manager=LoginManager(app)
-login_manager.login_view='login'
+
 
 # to config particular data
 #app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://username:password@localhost/databasename'
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/slot'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/slot'
 db=SQLAlchemy(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return user.query.get(int(user_id))
+
+
 
 class Test(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(50))
 
-class user(db.Model):
+class User(UserMixin,db.Model):
     uid=db.Column(db.Integer,primary_key=True)
     srfid=db.Column(db.String(20),unique=True)
     email=db.Column(db.String(20))
@@ -40,11 +40,10 @@ class user(db.Model):
 def home():
     return render_template("index.html")
 
-#to create particular routing for user login t
-#this return usersignup page
 @app.route("/usersignup")
 def usersignup():
     return render_template("usersignup.html")
+
 
 @app.route("/userlogin")
 def userlogin():
@@ -53,20 +52,36 @@ def userlogin():
 #/signup happen in main.py file
 @app.route("/signup",methods=['POST','GET'])
 def signup():
-    if request.method=='POST':
+    if request.method=="POST":
         srfid=request.form.get('srf')
         email=request.form.get('email')
         dob=request.form.get('dob')
         #print(srfid,email,dob)
         encpassword=generate_password_hash(dob)
-         # new_user=db.engine.execute(f"INSERT INTO `user` (`srfid`,`email`,`dob`) VALUES ('{srfid}','{email}','{encpassword}') ")
+        from sqlalchemy import create_engine
+        
+        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+        engine=engine.connect()
+        #new_user=db.engine.execute(f"INSERT INTO `user` (`srfid`,`email`,`dob`) VALUES ('{srfid}','{email}','{encpassword}') ")
+        new_user=User(srfid=srfid,email=email,dob=dob)
+        db.session.add(new_user)
+        db.session.commit()
+                
+       
+    
         return 'USER added'
-        return render_template("/usersignup.html")
+        
+       
+
+    return render_template("usersignup.html")
         
 
         
 
 
+
+
+   
 
 
 # testing whether db is connected or not
